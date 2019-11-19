dd9d6cd2ecd83ea23a95a19106446ca23ec5d37e
brandnn <brandnn@uw.edu>
2019-11-18 20:45:07 -0800

    initializing sequelize

---
+++ models/patient.js
@@ -0,0 +1,49 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+    const Patient =  sequelize.define('patient', {
+      First_Name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      Last_Name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      Date_Of_Birth: {
+        type: DataTypes.DATE(),
+        allowNull: false
+      },
+      Address: {
+          type: DataTypes.STRING(50),
+          allowNull: false
+      },
+      City: {
+          type:DataTypes.STRING(50),
+          allowNull:false
+      },
+      State: {
+        type: DataTypes.STRING(2),
+        allowNull: false
+      },
+      Zip_Code:{
+        type: DataTypes.INTEGER(5),
+        allowNull:false
+      },
+      Email: {
+        type: DataTypes.STRING(50),
+        isEmail: true
+      },
+      orgID: {
+        type: DataTypes.INTEGER(11),
+        references: 'organizations', // <<< Note, its table's name, not object name
+        referencesKey: 'id' // <<< Note, its a column name
+      },
+      roomID: {
+        type: DataTypes.INTEGER(11),
+        references: 'rooms', // <<< Note, its table's name, not object name
+        referencesKey: 'id' // <<< Note, its a column name
+      }
+    });
+    return Patient;
+  };
---
+++ models/room.js
@@ -0,0 +1,29 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+    const Room =  sequelize.define('room', {
+      Name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      Address: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+      }, 
+    orgID: {
+        type: DataTypes.INTEGER(11),
+        references: 'organizations', // <<< Note, its table's name, not object name
+        referencesKey: 'id' // <<< Note, its a column name
+      },
+      patientID: {
+        type: DataTypes.INTEGER(11),
+        references: 'patients', // <<< Note, its table's name, not object name
+        referencesKey: 'id' // <<< Note, its a column name
+      },
+      Available: {
+          type: DataTypes.BOOLEAN(),
+          defaultValue: '1'
+      }
+    });
+    return Room;
+  };
