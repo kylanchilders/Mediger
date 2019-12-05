ce819cc0179445fb9789030b4112b37626e3541e
T3k5upp0rt <kylan.childers@gmail.com>
2019-12-05 11:14:08 -0800

    changed gitignore

---
+++ client/build/precache-manifest.96eb919ba0a129be1dd01fb3c45c27b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c679f62de33d13c85b0d8eb736018d03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b74ce803fd6e4f2f658d",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6e4c5ddbd882ea82e967",
+    "url": "/static/css/main.c71697c8.chunk.css"
+  },
+  {
+    "revision": "b74ce803fd6e4f2f658d",
+    "url": "/static/js/2.983f113e.chunk.js"
+  },
+  {
+    "revision": "6e4c5ddbd882ea82e967",
+    "url": "/static/js/main.fe06bceb.chunk.js"
+  },
+  {
+    "revision": "9824ede6d794b04c6a09",
+    "url": "/static/js/runtime-main.994d87ea.js"
+  },
+  {
+    "revision": "7763863297c463779c3ef444aea04033",
+    "url": "/static/media/carouselImage14.77638632.jpeg"
+  },
+  {
+    "revision": "bdf15a1aa5c84eeaabc4913703df3d05",
+    "url": "/static/media/carouselImage15.bdf15a1a.png"
+  },
+  {
+    "revision": "1f040bb4e0164801613740ee3aeac9ec",
+    "url": "/static/media/carouselImage2.1f040bb4.png"
+  },
+  {
+    "revision": "a267d40bc87f6a93dd4b6037f397c2ad",
+    "url": "/static/media/carouselImage3.a267d40b.jpeg"
+  },
+  {
+    "revision": "5c020bd5f439b7952ae817951ec2eeb2",
+    "url": "/static/media/loading.5c020bd5.svg"
+  }
+]);
