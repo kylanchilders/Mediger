b7db5cb8da5de501cfde835b2859ebe04e2d0690
T3k5upp0rt <kylan.childers@gmail.com>
2019-12-05 14:30:25 -0800

    updated development sql string

--- config/config.js
+++ config/config.js
@@ -1,11 +1,7 @@
 module.exports = 
 {
   "development": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_KEY,
-    "database": process.env.MYSQL_DBNAME,
-    "host": process.env.MYSQL_HOST,
-    "port": 3306,
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   },
   "test": {
@@ -17,7 +13,7 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-"use_env_variable": "JAWSDB_URL",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
