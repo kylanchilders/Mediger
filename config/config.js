48e69a3383dfdab89af418182ea2b3b829dc8a12
T3k5upp0rt <kylan.childers@gmail.com>
2019-12-05 18:45:52 -0800

    blah

--- config/config.js
+++ config/config.js
@@ -1,7 +1,11 @@
 module.exports = 
 {
   "development": {
-    "use_env_variable": "JAWSDB_URL",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "port": 3306,
     "dialect": "mysql"
   },
   "test": {
